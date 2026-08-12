288903854c75f355ca50a2c44bd6d42aa7423757
Brooks Cole <marinjyang@outlook.com>
2026-08-12 16:45:09 +0800

    1、学习python

--- src/my_project/main.py
+++ src/my_project/main.py
@@ -1,3 +1,3 @@
 # 从当前包内导入（绝对导入，清晰且稳定）测试
-from src.my_project.utils.dbUtil import conn, cursor 
+from src.my_project.utils.db_util import conn, cursor
 
---
+++ src/my_project/utils/practical_small_tools.py
@@ -0,0 +1,20 @@
+# 1、打印语句
+# print("Hello World")
+
+# 2、控制台输入内容，获取的是字符串内容。
+# a = input("请输入内容:\n")
+# 3、把字符串转换成int类型。
+# a = int(input())
+# b = int(input())
+
+# 4、float()转换成小数
+# a = float(input())
+# b = float(input())
+# c = a * b + a
+# 5、f可以给{变量}赋值。
+# name = "张三"
+# age = '20'
+# print(f"姓名：{name}\n年龄：{age}")
+
+
+
